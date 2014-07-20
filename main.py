43b3d92b3f2cd810a46a59b5769248280b6c63a9
Jonathan Metzman <jon.metzman@gmail.com>
2014-07-20 13:54:38 -0400

    Install packages on android device

---
+++ main.py
@@ -0,0 +1,31 @@
+import os
+from com.android.monkeyrunner import MonkeyRunner, MonkeyDevice
+
+def get_MainActivity(package_name):
+    return package_name + 'MainActivity'
+
+def install_package(package_path, device):
+    if device.installPackage(package_path):
+        return True
+    else:
+        return False
+
+def install_packages(device, directory='apk'):
+    apks = os.listdir(directory)
+    installation_results = []
+    for apk in apks:
+        package_path = os.getcwd() + '/' + directory + '/' + apk
+        print package_path
+        result = install_package(package_path, device)
+        installation_results.append(result)
+    return installation_results
+
+def main():
+    timeout = 10000
+    device = MonkeyRunner.waitForConnection(timeout)
+    directory = 'apk'
+    packages = install_packages(device, directory=directory)
+    print packages
+
+if __name__=='__main__':
+    main()
